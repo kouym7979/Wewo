7a4447d3710bd1466a013db5d601e44a208e28db
kouym7979 <52284829+kouym7979@users.noreply.github.com>
2019-11-09 09:26:47 +0900

    register 및 login 연동
    login할시에 연동이 안되는 오류가 발견 되었습니다. 그에 대한 수정사항과 파이어베이스에 등록되는 것을 확인하고 올립니다.

--- Project_Wewo/app/src/main/java/com/example/project_wewo/ui/login/LoginActivity.java
+++ Project_Wewo/app/src/main/java/com/example/project_wewo/ui/login/LoginActivity.java
@@ -37,13 +37,10 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();//파이어베이스 선언
-
     private LoginViewModel loginViewModel;
-    EditText usernameEditText = findViewById(R.id.username);
-    EditText passwordEditText = findViewById(R.id.password);
-    final Button loginButton = findViewById(R.id.login);
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();//파이어베이스 선언
     String Email, PW;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,49 +49,142 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        Email = usernameEditText.getText().toString();//입력받은 email을 문자열로 저장
-        PW = passwordEditText.getText().toString();//입력받은 pw을 문자열로 저장
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        //final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-        final Button registerButton = findViewById(R.id.register); //레지스터 버튼
+        //final Button registerButton = findViewById(R.id.register); //레지스터 버튼
 
-        loginButton.setOnClickListener(this);
+        //loginButton.setOnClickListener(this);
+        findViewById(R.id.login).setOnClickListener(this);
+        findViewById(R.id.register).setOnClickListener(this);
+        Email=usernameEditText.getText().toString();
+        PW=passwordEditText.getText().toString();
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+       /* loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(@Nullable LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getUsernameError() != null) {
+                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+*/
+        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+            @Override
+            public void onChanged(@Nullable LoginResult loginResult) {
+                if (loginResult == null) {
+                    return;
+                }
+                loadingProgressBar.setVisibility(View.GONE);
+                if (loginResult.getError() != null) {
+                    showLoginFailed(loginResult.getError());
+                }
+                if (loginResult.getSuccess() != null) {
+                    updateUiWithUser(loginResult.getSuccess());
+                }
+                setResult(Activity.RESULT_OK);
+
+                //Complete and destroy login activity once successful
+                finish();
+            }
+        });
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
+                        passwordEditText.getText().toString());
+            }
+        };
+        usernameEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginViewModel.login(usernameEditText.getText().toString(),
+                            passwordEditText.getText().toString());
+                }
+                return false;
+            }
+        });
+
+
+
+       /* registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent rintent = new Intent(
+                Intent rintent= new Intent(
                         getApplicationContext(),
                         MemberRegisterActivity.class);
                 startActivity(rintent);
             }
-        });
+        });*/
+
+
+
+
+    }
+
+    private void updateUiWithUser(LoggedInUserView model) {
+        String welcome = getString(R.string.welcome) + model.getDisplayName();
+        // TODO : initiate successful logged in experience
+        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    }
+
+    private void showLoginFailed(@StringRes Integer errorString) {
+        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onClick(View v) {//로그인 버튼 누를시에 작동
-        mAuth.signInWithEmailAndPassword(Email, PW)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.login:
+                mAuth.signInWithEmailAndPassword(Email, PW)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(LoginActivity.this,"Login Success",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this,Main_home.class));//성공하면 홈화면으로 이동
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Login error.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                // ...
+                            }
+                        });
+                break;
+            case R.id.register:
+                Intent rintent= new Intent(
+                        getApplicationContext(),
+                        MemberRegisterActivity.class);
+                startActivity(rintent);
+                break;
 
-                            FirebaseUser user = mAuth.getCurrentUser();
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-
-                            Toast.makeText(LoginActivity.this, "Login error.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-                        Intent intent= new Intent(
-                                getApplicationContext(),
-                                Main_home.class);
-                        startActivity(intent);
-                    }
-                });
+        }
     }
-
-
 }
