9b4b1b17b30c9d7dda33b8f31c411c851eca3c3b
kouym7979 <52284829+kouym7979@users.noreply.github.com>
2019-11-11 23:22:09 +0900

    [add] 스플래시 화면적용
    스플래시 화면을 적용하고  회원가입시와 로그인화면의 레이아웃을 다듬었습니다.

--- FinalP/app/src/main/java/com/example/finalp/SignupActivity.java
+++ FinalP/app/src/main/java/com/example/finalp/SignupActivity.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText sEmail, sPw;
+    private EditText sEmail, sPw,sPw_chk;
     private FirebaseAuth mAuth =FirebaseAuth.getInstance();
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
     @Override
@@ -31,38 +31,40 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         sEmail=findViewById(R.id.semail);
         sPw=findViewById(R.id.spw);
-
+        sPw_chk=findViewById(R.id.spw_chk);
         findViewById(R.id.btn_signup).setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        mAuth.createUserWithEmailAndPassword(sEmail.getText().toString(),sPw.getText().toString())
-                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Map<String, Object> userMap = new HashMap<>();//유저의 id,pw를 저장하기 위해 맵으로 선언
-                            //FirebaseID란 클래스를 추가로 만들어서 private public string 형식으로 id,pw 값을 저장하는 클래스 생성
-                            userMap.put(FirebaseID.documentId, user.getUid());//email받을까
-                            userMap.put(FirebaseID.email,sEmail.getText().toString());//고유아이디 설정 해줌
-                            userMap.put(FirebaseID.password,sPw.getText().toString());//pw받고
-                            //mStore.collection("users").document(user.getUid()).set(userMap, SetOptions.merge());
-                            mStore.collection("users")
-                                    .add(user);
-                            finish();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(SignupActivity.this, "Signup error.",
-                                    Toast.LENGTH_SHORT).show();
+        //비밀번호와 비밀번호 확인이 일치하고 이메일이 공란이 아닐때 조건을 추가해야함
+            mAuth.createUserWithEmailAndPassword(sEmail.getText().toString(), sPw.getText().toString())
+                    .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Map<String, Object> userMap = new HashMap<>();//유저의 id,pw를 저장하기 위해 맵으로 선언
+                                //FirebaseID란 클래스를 추가로 만들어서 private public string 형식으로 id,pw 값을 저장하는 클래스 생성
+                                userMap.put(FirebaseID.documentId, user.getUid());//email받을까
+                                userMap.put(FirebaseID.email, sEmail.getText().toString());//고유아이디 설정 해줌
+                                userMap.put(FirebaseID.password, sPw.getText().toString());//pw받고
+                                //mStore.collection("users").document(user.getUid()).set(userMap, SetOptions.merge());
+                                mStore.collection("users")
+                                        .add(user);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignupActivity.this, "Signup error.",
+                                        Toast.LENGTH_SHORT).show();
 
+                            }
+
+                            // ...
                         }
-
-                        // ...
-                    }
-                });
+                    });
+        }
     }
-}
+
