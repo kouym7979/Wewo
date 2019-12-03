5ad91eb5551542be0b63ac31422b6f43d81819b4
Kyun2da <dktnskdktnsk@gmail.com>
2019-12-04 00:22:39 +0900

    [update] login layout, toolbar

--- FinalP/app/src/main/java/com/example/finalp/ui/settings/SettingsFragment.java
+++ FinalP/app/src/main/java/com/example/finalp/ui/settings/SettingsFragment.java
@@ -1,17 +1,17 @@
 package com.example.finalp.ui.settings;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.EditText;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+        import androidx.annotation.NonNull;
+        import androidx.fragment.app.Fragment;
 
-import com.example.finalp.R;
-import com.example.finalp.modifyPassword;
+        import com.example.finalp.R;
+        import com.example.finalp.modifyPassword;
 
 public class SettingsFragment extends Fragment{
 
