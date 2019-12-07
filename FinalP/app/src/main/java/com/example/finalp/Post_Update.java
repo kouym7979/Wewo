6cf611be626bfd89caae317e7c565c5703dcc3ea
kouym7979 <52284829+kouym7979@users.noreply.github.com>
2019-12-07 20:51:17 +0900

    [add]Post_update function and Post_delete Fuction
    [add]Post_update function and Post_delete Fuction

--- FinalP/app/src/main/java/com/example/finalp/Post_Update.java
+++ FinalP/app/src/main/java/com/example/finalp/Post_Update.java
@@ -12,17 +12,11 @@ import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -35,14 +29,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-
-public class Post_write extends AppCompatActivity implements View.OnClickListener {
+public class Post_Update extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth=FirebaseAuth.getInstance();//사용자 정보 가져오기
     private FirebaseFirestore mStore=FirebaseFirestore.getInstance();
@@ -59,7 +51,7 @@ public class Post_write extends AppCompatActivity implements View.OnClickListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_write);
+        setContentView(R.layout.activity_post__update);
 
         mTitle=findViewById(R.id.Post_write_title);//제목 , item_post.xml의 변수와 혼동주의
         mContents=findViewById(R.id.Post_write_contents);
@@ -69,18 +61,13 @@ public class Post_write extends AppCompatActivity implements View.OnClickListene
         post_imageView.setVisibility(View.INVISIBLE);
 
         if(mAuth.getCurrentUser()!=null){//UserInfo에 등록되어있는 닉네임을 가져오기 위해서
-            mStore.collection("user").document(mAuth.getCurrentUser().getUid())// 여기 콜렉션 패스 경로가 중요해 보면 패스 경로가 user로 되어있어서
-                    //우리 파이어베이스의 user 컬렉션의 정보를 가져올 수 있어. email, password, Uid(주민번호같은 거), nickname
-                    //현재는 user에는 저 4개의 정보밖에 없어. 만약 다른 정보를 추가로 받고싶다면 어제 말했듯이 너가 가입에서부터 새로 입력값을 받고
-                    //FirebaseID에 똑같이 추가하고 SIgnupActivity에 UserMap부분을 수정하면되. 아마 UserInfo에 객체 생성은 따로 안해줘도 될거야 내가 형식을 바꿔놔서
+            mStore.collection("user").document(mAuth.getCurrentUser().getUid())//
                     .get()
                     .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                             if(task.getResult()!=null){
                                 p_nickname=(String)task.getResult().getData().get(FirebaseID.nickname);//
-                                //닉네임 뿐만아니라 여기서 FirebaseID.password를 하면 비밀번호도 받아올 수 있음. 즉 원하는 것을 넣으면 됨
-                                //파이어베이스에 등록된 닉네임을 불러옴
                                 writer_id=(String)task.getResult().getData().get(FirebaseID.documentId);
                                 Log.d("확인","현재 사용자 uid입니다:"+writer_id);
                             }
@@ -141,7 +128,7 @@ public class Post_write extends AppCompatActivity implements View.OnClickListene
 
         if(uriProfileImage !=null)
         {
-           // progressBar.setVisibility(View.VISIBLE);
+            // progressBar.setVisibility(View.VISIBLE);
             profileImageRef.putFile(uriProfileImage)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
@@ -179,8 +166,7 @@ public class Post_write extends AppCompatActivity implements View.OnClickListene
         if(mAuth.getCurrentUser()!=null){
             String PostID=mStore.collection("Post").document().getId();//제목이 같아도 게시글이 겹치지않게
             Intent intent=getIntent();
-            post_num=intent.getStringExtra("post");
-
+            post_num=intent.getStringExtra("number");
             Log.d("확인","여기는 게시글 작성:"+post_num);
             Map<String,Object> data=new HashMap<>();
             data.put(FirebaseID.documentId,mAuth.getCurrentUser().getUid());//유저 고유번호
@@ -189,15 +175,19 @@ public class Post_write extends AppCompatActivity implements View.OnClickListene
             data.put(FirebaseID.timestamp, FieldValue.serverTimestamp());//파이어베이스 시간을 저장 그래야 게시글 정렬이 시간순가능
             data.put(FirebaseID.nickname,p_nickname);
             data.put(FirebaseID.p_photo,photoUrl);
-            //data.put(FirebaseID.nickname,p_nickname);
-            data.put(FirebaseID.post_id,PostID);//게시글 ID번호
             data.put(FirebaseID.post_num,post_num);
+            data.put(FirebaseID.post_id,PostID);//게시글 ID번호
             data.put(FirebaseID.writer_id,writer_id);
             if(!TextUtils.isEmpty(postImageUrl))
             {
                 data.put(FirebaseID.post_photo,postImageUrl);
             }
-            mStore.collection("Post").document(PostID).set(data);//Post라는 테이블에 데이터를 입력하는것/ 문서 이름을 PostID로 등록
+            mStore.collection("Post").document(intent.getStringExtra("Postid")).update(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                }
+            });//Post라는 테이블에 데이터를 입력하는것/ 문서 이름을 PostID로 등록
+            //startActivity(new Intent(this,Post_Comment.class));
             finish();
         }
     }
